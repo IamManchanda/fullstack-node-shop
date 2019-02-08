004f4cf4e01331c9a7a2a91e19c512deb7126524
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-02-09 01:49:08 +0530

    Working Pass on adding Pug within the workflow!

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -1,16 +1,13 @@
 /* Routes => Shop.js */
 
 const express = require('express')
-const path = require('path');
 
-const rootDir = require('../utils/path');
 const products = require('./admin').products;
 
 const router = express.Router();
 
 router.get('/', function homePageMiddleware(request, response, next) {
-  console.log({ products });
-  response.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  response.render('shop', { products, path: '/', documentTitle: `Harry's Shop` });
 });
 
 module.exports = router;
