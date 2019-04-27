c474f5afc0055460f29edd5f751149bd286131d0
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-04-27 14:48:25 +0530

    Chore: Code styling of kebab convertor util

--- src/util/convertToKebabCase.js
+++ src/util/convertToKebabCase.js
@@ -1,9 +1,8 @@
 // Util - convertToKebabCase 
 // Source: https://gist.github.com/thevangelist/8ff91bac947018c9f3bfaad6487fa149#gistcomment-2183914
-const convertToKebabCase = (title) =>
-  title
-    .replace(/([a-z])([A-Z])/g, '$1-$2')    // get all lowercase letters that are near to uppercase ones
-    .replace(/[\s_]+/g, '-')  // replace all spaces and low dash
-    .toLowerCase()  // convert to lower case
+const convertToKebabCase = title => title
+  .replace(/([a-z])([A-Z])/g, '$1-$2')    // get all lowercase letters that are near to uppercase ones
+  .replace(/[\s_]+/g, '-')  // replace all spaces and low dash
+  .toLowerCase();  // convert to lower case
 
 module.exports = convertToKebabCase;
