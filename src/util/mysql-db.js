81a72cbd78e4e12eca4932b1f82adf641559936c
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-04-28 16:07:15 +0530

    Move other important db details beside pass also in env!

--- src/util/mysql-db.js
+++ src/util/mysql-db.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2');
 
+const host = process.env.MYSQL_HOST;
+const user = process.env.MYSQL_USER;
+const database = process.env.MYSQL_DATABASE;
 const password = process.env.MYSQL_PASSWORD;
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'fullstack-node-shop',
+  host,
+  user,
+  database,
   password,
 });
 
